0965b36d4ac006fc36cad6539e088fe1b563542d
giovaneserv <giovan2003serv@gmail.com>
2024-09-21 00:09:11 -0300

    Arquivo principal App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import login from "./src/login";
-import cadastro from "./src/cadastro";
+import login from "./src/Telas/login";
+import cadastro from "./src/Telas/cadastro";
+import perfil from './src/Telas/perfil'
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -9,11 +10,13 @@ function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="cadastro">
+      <Stack.Navigator initialRouteName="perfil">
         <Stack.Screen name="login" component={login}
-          options={{ title:'Tela Login', headerStyle:{backgroundColor:'black'}, headerTintColor:'white' }} />
+          options={{ title:'Tela Login', headerStyle:{display:'none'}}} />
         <Stack.Screen name="cadastro" component={cadastro}
           options={{ title:'Tela cadastro', headerStyle:{backgroundColor:'black'}, headerTintColor:'white' }} />
+          <Stack.Screen name='perfil' component={perfil}
+          />
       </Stack.Navigator>
     </NavigationContainer>
 
