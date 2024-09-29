36d8ca17b2676d3d82496732302f6e75f03fed5d
giovaneserv <giovan2003serv@gmail.com>
2024-09-28 23:44:03 -0300

    Arquivo Principal App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Perfil from "./src/Telas/Perfil";
 import Cadastro from "./src/Telas/Cadastro";
 import Notificacoes from "./src/Telas/Notificacoes"
 import Postar from "./src/Telas/Postar";
+import Feed from "./src/Telas/Feed"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -12,16 +13,18 @@ function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Notificacoes">
+      <Stack.Navigator initialRouteName="Cadastro">
         <Stack.Screen name="Login" component={Login}
-          options={{ title: 'Login', headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
+          options={{headerShown: false, title: 'Login', headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
         <Stack.Screen name="Cadastro" component={Cadastro}
-          options={{ title: 'Perfil', headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
+          options={{headerShown: false, title: 'Perfil', headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
         <Stack.Screen name='Perfil' component={Perfil}
-          options={{ headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
+          options={{headerShown: false, headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
         <Stack.Screen name='Notificacoes' options={{ headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} component={Notificacoes} />
         <Stack.Screen name='Postar' 
-        options={{ title:'Postar', headerStyle: { backgroundColor: 'black'} , headerTintColor: 'white' }} component={Postar} />
+        options={{headerShown: false, title:'Feed', headerStyle: { backgroundColor: 'black'} , headerTintColor: 'white' }} component={Postar} />
+        <Stack.Screen name='Feed' 
+        options={{headerShown: false, title:'Feed', headerStyle: { backgroundColor: 'black'} , headerTintColor: 'white' }} component={Feed} />
       </Stack.Navigator>
     </NavigationContainer>
 
