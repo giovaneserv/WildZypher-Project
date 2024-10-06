cc67fa7d09be9ba8a59cc921f96956a62a8333d6
giovaneserv <giovan2003serv@gmail.com>
2024-10-06 19:40:56 -0300

    Arquivo principal App.js

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from "react";
 import Login from "./src/Telas/Login";
 import Perfil from "./src/Telas/Perfil";
 import Cadastro from "./src/Telas/Cadastro";
-import Notificacoes from "./src/Telas/Notificacoes"
 import Postar from "./src/Telas/Postar";
 import Feed from "./src/Telas/Feed"
+import HomeScreen from "./src/Telas/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -13,14 +13,15 @@ function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen}
+          options={{headerShown: false, title: 'Login', headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
         <Stack.Screen name="Login" component={Login}
           options={{headerShown: false, title: 'Login', headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
         <Stack.Screen name="Cadastro" component={Cadastro}
           options={{headerShown: false, title: 'Perfil', headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
         <Stack.Screen name='Perfil' component={Perfil}
           options={{headerShown: false, headerStyle: { backgroundColor: 'black' }, headerTintColor: 'white' }} />
-        <Stack.Screen name='Notificacoes' options={{ headerStyle: { headerShown:false, backgroundColor: 'black' }, headerTintColor: 'white' }} component={Notificacoes} />
         <Stack.Screen name='Postar' 
         options={{headerShown: false, title:'Feed', headerStyle: { backgroundColor: 'black'} , headerTintColor: 'white' }} component={Postar} />
         <Stack.Screen name='Feed' 
